14584bc1d58d8076e00c208133743e87a18a887b
leanachen <lwc3@cs.washington.edu>
2019-03-05 22:42:17 -0800

    minor bug fixes

--- test.java
+++ test.java
@@ -4,23 +4,24 @@ import java.util.Random;
 public class test {
 	public static final int WEEKS = 10;
 	public static final double BETA = 0.2;
-	
+        public static final int NUM_OF_TA = 10;
+
 	public static void main(String[] args) {
-		double[] quality = new double[10];
-		double[] initialQuality = getQuality(taQuality);
-		double[] estimate = new double[10];
+		double[] quality = new double[NUM_OF_TA];
+		double[] initialQuality = getQuality(quality);
+		double[] estimate = new double[NUM_OF_TA];
 		double[] initialEstimate = getEstimate(estimate);
 		for (int i = 0; i < WEEKS; i++) {
 			double[] alpha = Calculate.calculateAlpha(estimate, BETA);
 			estimate = Calculate.calculateEstimate(quality, estimate, alpha);
 		}
 		System.out.println("Initial estimates: " + Arrays.toString(initialEstimate));
-		System.out.println("Final estimates: " + Arrays.toString(initialEstimate));
+		System.out.println("Final estimates: " + Arrays.toString(estimate));
 		
 	}
 	
 	
-	public double[] getQuality(double[] copy) {
+	public static double[] getQuality(double[] copy) {
 		double[] ta = new double[10];
 		for (int i = 0; i < 9; i++) {
 			ta[i] = i / 1.;
@@ -29,7 +30,7 @@ public class test {
 		return ta;
 	}
 	
-	public double[] getEstimate(double[] copy) {
+	public static double[] getEstimate(double[] copy) {
 		double[] estimates = new double[10];
 		double standardDev = 0.2;
 		Random r = new Random();
